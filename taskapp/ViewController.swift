a8f59330d7d1bbd66bbd890c96c9f9aaa61a5510
mari.saito <mari.saito.tec@gmail.com>
2020-10-17 22:45:31 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by 齋藤万璃夢 on 2020/10/17.
+//  Copyright © 2020 mari.saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
